60a0096129faadbadbd046e95ca4f6cd4650d72f
Laila <lailarhcampos@gmail.com>
2025-07-16 19:06:04 -0300

    Add routing and create Home and Not Found pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,11 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import tailwindlogo from './assets/tailwind.svg'
 import './App.css'
+import AppRoutes from './routes/routes';
 
 const App: React.FC = () => {
 
   return (
     <>
-      <div className='flex justify-center gap-4'>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://tailwindcss.com" target="_blank">
-          <img src={tailwindlogo} className="logo tailwind scale-150" alt="Tailwind CSS logo" />
-        </a>
-      </div>
-      <div>
-
-      </div>
-      <h1>Vite + React + Tailwind CSS</h1>
-      <div className="card">
-        <p>
-          Template de projeto React TypeScript com Vite e Tailwind CSS
-        </p>
-      </div>
+      <AppRoutes />
     </>
   )
 }
